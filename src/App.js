2cb5fff690d3176ba9f96452e2ef39add1cfc18a
JosephSCha <josephshincha@gmail.com>
2020-06-28 16:56:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {Home, AboutMe, ContactMe, Projects, Resume, Sidebar} from './Components'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/aboutme">
+            <AboutMe/>
+          </Route>
+          <Route path="/projects">
+            <Projects/>
+          </Route>
+          <Route path="/contactme">
+            <ContactMe/>
+          </Route>
+          <Route path="/resume">
+            <Resume/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+
+        <Sidebar/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as Home} from './home'
+export { default as AboutMe} from './aboutme'
+export { default as ContactMe} from './contactme'
+export { default as Projects} from './projects'
+export { default as Resume} from './resume'
+export { default as Sidebar} from './sidebar'
