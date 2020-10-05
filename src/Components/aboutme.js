e60ff06581105429005ffc5decd317b7fc5f8412
JosephSCha <josephshincha@gmail.com>
2020-10-04 23:03:53 -0400

    about me fix

--- src/Components/aboutme.js
+++ src/Components/aboutme.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import pdf from './resume.pdf'
+// import pdf from './resume.pdf'
 
 function AboutMe() {
     return (
@@ -7,15 +7,17 @@ function AboutMe() {
             <h1>Joseph S Cha</h1>
             <img className="profile-picture" src="./profilepicture.jpg" alt="default"/>
                 <div className="profile-info">         
-                    June 2020 graduate from Flatiron Schools Software Engineering Program.
-                    Fullstack Developer Experienced in Ruby and JavaScript based programming and frameworks such as Rails and ReactJS/React native. 
+                    Flatiron School Graduate
+                    <br></br>
+                    Experienced in Ruby and JavaScript based programming and frameworks such as Rails and ReactJS/React native. 
+                    <br></br>
                     Expert in Project Management and Problem Solving with a driven mind-set seeking like-minded teams and companies to change the world through code. 
                 </div>
                 <div className="portfolio-techstack">
-                    Languages: JavaScript (ES5/ES6), Ruby, HTML, CSS, SQL
-                    Technologies | Frameworks | Libraries: Ruby on Rails, React, React Native, Redux, Node.js, PostgreSQL, SQLite3, Bootstrap, Semantic-UI
+                Technologies | Frameworks | Libraries: JavaScript (ES5/ES6), Ruby, HTML, CSS, SQL
+                    Ruby on Rails, React, React Native, Redux, Node.js, PostgreSQL, SQLite3, Bootstrap, Semantic-UI
                 </div>
-                <a href={pdf}>Resume</a>
+                {/* <a href={pdf}>Resume</a> */}
         </div>
     )
 }
