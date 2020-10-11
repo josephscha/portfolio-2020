786ed45252cdf31f8d1607e60e375e8ce3bc4929
JosephSCha <josephshincha@gmail.com>
2020-10-11 19:10:46 -0400

    added icons to nav

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -12,7 +12,16 @@ function Navigation() {
                 <NavLink className="d-inline p-2 bg-dark text-white" to='/projects'>Projects</NavLink>
                 <NavLink className="d-inline p-2 bg-dark text-white" to='/contactme'>Contact Me</NavLink>
                 <NavLink className="d-inline p-2 bg-dark text-white" to='/resume'>Resume</NavLink>
-                
+                <a 
+                href="https://www.linkedin.com/in/joseph-cha-316639174/" 
+                target="_blank" 
+                rel="noopener noreferrer"><FaLinkedin/>
+            </a>
+            <a 
+                href="https://github.com/josephscha" 
+                target="_blank" 
+                rel="noopener noreferrer"><FaGithub/>
+            </a>
             </Nav>
         </Navbar>
     )
